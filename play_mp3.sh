f0ab353a421a71351555c1136178feab98134fe2
jithinag91 <jithinag91@gmail.com>
2017-04-22 00:39:30 +0530

    Add files via upload

--- play_mp3.sh
+++ play_mp3.sh
@@ -5,4 +5,4 @@ then fq=$1
 	file_location=$2
 fi
 
-sox -t mp3 $file_location -t wav -r 22050 stereo -c 1 -|./fm_transmitter -f $fq - && echo 'runsuccess' || echo "error fm_transmitter"
+sox -t mp3 $file_location -t wav -r 22050 -c 1 -|./fm_transmitter -f $fq - && echo 'runsuccess' || echo "error fm_transmitter"
